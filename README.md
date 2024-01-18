cfb9d93954254f9f41aa6e09e36b9c1edcab2a40
lleadbet <5595530+lleadbet@users.noreply.github.com>
2024-01-17 20:50:30 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# Embedify Discord Bot
+
+## What does this bot do? 
+
+There are too many damn services now blocking embeds, and so it's a quick bot to fix them with publicly available embed proxies. 
+
+## How do I add?
+To add to your channel: [https://discord.com/api/oauth2/authorize?client_id=1197310892866539671&permissions=274877925440&scope=bot](https://discord.com/api/oauth2/authorize?client_id=1197310892866539671&permissions=274877925440&scope=bot).
+
+## Self-hosting
+
+If you'd like to self-host, simply provide a `DISCORD_TOKEN` in a `.env` file (sample included). Review the Discord documentation for information on how to get started with building a Discord Bot. Otherwise, feel free to use the self-hosted version. 
+
+## Development
+
+Feel free to submit PRs if you like, such as if you find that a given embed stops working for one reason or another, or you'd like to add another domain. 
---
+++ main.go
@@ -0,0 +1,114 @@
+package main
+
+import (
+	"fmt"
+	"net/url"
+	"os"
+	"os/signal"
+	"regexp"
+	"syscall"
+
+	"github.com/bwmarrin/discordgo"
+	_ "github.com/joho/godotenv/autoload"
+)
+
+type DomainProps struct {
+	Domain        string
+	RequiredPaths []*regexp.Regexp
+}
+
+var REACTION_EMOJI = "concreteBONK:959613362612887582"
+
+var HANDLED_DOMAINS = map[string]DomainProps{
+	"twitter.com": {
+		Domain:        "vxtwitter.com",
+		RequiredPaths: []*regexp.Regexp{regexp.MustCompile(`\/.*\/status\/`)},
+	},
+	"x.com": {
+		Domain:        "fixvx.com",
+		RequiredPaths: []*regexp.Regexp{regexp.MustCompile(`\/.*\/status\/`)},
+	},
+	"instagram.com": {
+		Domain:        "ddinstagram.com",
+		RequiredPaths: []*regexp.Regexp{regexp.MustCompile(`\/p\/`)},
+	},
+	"tiktok.com": {
+		Domain:        "vxtiktok.com",
+		RequiredPaths: []*regexp.Regexp{regexp.MustCompile(`\/t\/`)},
+	},
+}
+
+func main() {
+	if os.Getenv("DISCORD_TOKEN") == "" {
+		panic("DISCORD_TOKEN is not set")
+	}
+	if os.Getenv("REACTION_EMOJI") != "" {
+		REACTION_EMOJI = os.Getenv("REACTION_EMOJI")
+	}
+
+	dgo, err := discordgo.New("Bot " + os.Getenv("DISCORD_TOKEN"))
+	if err != nil {
+		panic(err)
+	}
+
+	dgo.AddHandler(messageCreate)
+
+	err = dgo.Open()
+	if err != nil {
+		panic(err)
+	}
+
+	println("Bot is running...")
+	defer dgo.Close()
+
+	// Create a channel to receive the SIGINT signal
+	sigint := make(chan os.Signal, 1)
+	signal.Notify(sigint, os.Interrupt, syscall.SIGINT, syscall.SIGTERM)
+
+	// Block until a signal is received
+	<-sigint
+}
+
+func messageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
+	if m.Author.Bot {
+		return
+	}
+
+	urlRegex := regexp.MustCompile(`https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-z]{2,4}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)`)
+	tldRegex := regexp.MustCompile(`\.?([^.]*.com)`)
+
+	matches := urlRegex.FindAllStringSubmatch(m.Content, -1)
+	var content = ""
+	for _, match := range matches {
+		url, err := url.Parse(match[0])
+		if err != nil {
+			fmt.Printf("%s\n", err)
+			continue
+		}
+		tld := tldRegex.FindStringSubmatch(url.Hostname())[1]
+		if val, ok := HANDLED_DOMAINS[tld]; ok {
+			for _, path := range val.RequiredPaths {
+				if ok := path.MatchString(url.Path); ok {
+					url.Host = val.Domain
+					content += fmt.Sprintf("%s\n", url.String())
+					break
+				}
+			}
+
+		}
+	}
+	if content == "" {
+		return
+	}
+	_, err := s.ChannelMessageSend(m.ChannelID, content)
+	if err != nil {
+		fmt.Printf("%s\n", err)
+		return
+	}
+
+	err = s.MessageReactionAdd(m.ChannelID, m.ID, REACTION_EMOJI)
+	if err != nil {
+		fmt.Printf("%s\n", err)
+		return
+	}
+}
